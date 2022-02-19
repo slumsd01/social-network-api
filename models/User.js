758596f1d3dc77ebf269a855f8a19d02dffef82e
slumsd01 <90315740+slumsd01@users.noreply.github.com>
2022-02-19 13:46:14 -0500

    fixed connection error

--- models/User.js
+++ models/User.js
@@ -1,9 +1,15 @@
 const {Schema, model} = require('mongoose')
 
-const UserSchema = new Schema({
-    username: {},
-    email: {},
-})
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String
+        },
+        email: {
+            type: String
+        },
+    }
+)
 
 const User = model('User', UserSchema)
 
--- server.js
+++ server.js
@@ -4,11 +4,17 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-network', {
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(require('./routes'))
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
 mongoose.set('debug', true);
 
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
